e4ec4499e655d82a874e2fa6b3182970c1541c0a
HarshaiOS1 <46565732+HarshaiOS1@users.noreply.github.com>
2024-05-17 12:06:44 +0200

    created project

---
+++ TCALearning/TCALearning/TCALearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCALearningApp.swift
+//  TCALearning
+//
+//  Created by Krishnappa, Harsha on 17/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct TCALearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
