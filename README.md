1b71787db3b6bd0a05586618da409d401f046a2f
HarshaiOS1 <46565732+HarshaiOS1@users.noreply.github.com>
2024-05-21 11:00:02 +0200

    remove depricated usage
    used observableobject viewstore to build ui

--- README.md
+++ README.md
@@ -6,14 +6,16 @@ Essential components :
  
 1. State: State is a struct that defines the data needed for the logic and UI (PTR : Its funny in fltuter & koltin jetpack this defines the user state of the componenet) (for understanding we can consider this to be sate of the data at that point!)
     a. added model for parsing as well
-    
 2. Action: Action is an enum that defines various actions trigerred from many sources, such as user, timer calls & api request results
-3. Reducer: Reducer is a func that determines how actions will be processed and updates the state based on those actions (business logic here to handle action)
+3. Reducer: Reducer is a func that determines how actions will be processed and updates the state based on those actions (business logic here to handle action). Reducer contains
 4. Effects: An event that modifies the state outside of its local context (let's see how in demo)
 5. Store (ViewStore): The store manages all of the above. It receives an action and runs the reducer to modify the state.
 
 
 Advantages : 
+1. strict unidirectional data flow.
+2. Modularity: encourages breaking down of application into smaller, composable components
+3. 
 
 Disadvantages: 
 1. still in evolution mode
--- TCALearning/TCALearning/BooksListView.swift
+++ TCALearning/TCALearning/BooksListView.swift
@@ -9,35 +9,43 @@ import SwiftUI
 import ComposableArchitecture
 
 struct BooksListView: View {
+    /*
+     depricated WithViewStore method update, use ObservedObject viewStore
+     */
     let store: StoreOf<BookListReducer>
+    @ObservedObject var viewStore: ViewStoreOf<BookListReducer>
     
+    init(store: StoreOf<BookListReducer>) {
+        self.store = store
+        self.viewStore = ViewStore(store, observe: { $0 })
+    }
     
     var body: some View {
         NavigationView {
-            WithViewStore(self.store, observe: { $0 }) { viewStore in
-                List {
-                    if let books = viewStore.books {
-                        ForEach(books, id:  \.isbn) { book in
-                            Text(book.name ?? "")
-                        }
-                    } else {
-                        Text("Book list is empty")
+            //            WithViewStore(self.store, observe: { $0 }) { viewStore in
+            List {
+                if let books = viewStore.books {
+                    ForEach(books, id:  \.isbn) { book in
+                        Text(book.name ?? "")
+                    }
+                } else {
+                    Text("Book list is empty")
+                }
+            }
+            
+            .navigationTitle("Books")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar(content: {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button(action: {
+                        viewStore.send(.fetchBooks)
+                    }) {
+                        Image(systemName: "arrow.clockwise.square")
                     }
                 }
-                
-                .navigationTitle("Books")
-                .navigationBarTitleDisplayMode(.inline)
-                .toolbar(content: {
-                    ToolbarItem(placement: .topBarTrailing) {
-                        Button(action: {
-                            viewStore.send(.fetchBooks)
-                        }) {
-                            Image(systemName: "arrow.clockwise.square")
-                        }
-                    }
-                })
-            }
+            })
         }
+        //        }
     }
 }
 
